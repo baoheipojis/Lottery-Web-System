d62209f62583fa50616af8ff67a1f8c38f1d664c
Pojis <221220001@smail.nju.edu.cn>
2025-03-23 17:52:27 +0800

    删除逻辑没有问题，只是因为我忘记把后端重新运行了而已。

--- lottery/src/main/java/com/example/lottery/repository/PrizeRepository.java
+++ lottery/src/main/java/com/example/lottery/repository/PrizeRepository.java
@@ -4,7 +4,11 @@ import com.example.lottery.entity.Prize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PrizeRepository extends JpaRepository<Prize, Long> {
     // 基本CRUD功能已由JpaRepository提供，无需额外定义
+    List<Prize> findByRarityAndFiveStarType(int rarity, String fiveStarType);
+    List<Prize> findByRarity(int rarity);
 }
